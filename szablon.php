5c92bffa48faabb03efcb5b7d7f3e15113b93b22
przemoxe <przemeqknight@interia.eu>
2021-10-25 19:53:02 +0200

    initial commit

---
+++ szablon.php
@@ -0,0 +1,191 @@
+<?php get_header(); ?>
+    <section class="intro">
+        <div class="col-container py-5">
+            <div class="row logo">
+                <div class="col">
+                    <div class="row d-flex justify-content-end ps-3 ps-md-0 pb-5 ">
+                        <div class="col-md-11 col-lg-9 d-flex align-items-center ">
+                            <img src="http://cut2code.test/wp-content/uploads/2021/10/make-up.png" alt="">
+                            <h5><?php echo get_field('one_name'); ?></h1>
+                            <span style="padding-left:10%; font-weight:600;">Customers <span style="color:#611dd4; ">Call Us:</span>1234556790</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-end align-items-center ps-3 ps-md-0">
+                <div class=" col-md-6 col-xl-4">
+                    <div class="row d-flex justify-content-end ">
+                        <h4>Download Free</h4>
+                        <h1>Landing Pages</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. onsectetur adipisicing elit. Sunt, nostrum fuga! Explicabo ex assumenda dolores, aliquid architecto iure tenetur cumque?</p>
+                        <div>
+                            <a href="" class="btn-intro">Download</a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-5 circle-container pt-5 pt-md-0 d-flex justify-content-start justify-content-sm-end pe-5 justify-content-md-start">
+                    <div class="circle circle-white-big">
+                        <div class="circle circle-purple-dark">
+                            <div class="circle circle-purtple-white">
+                                <div class="circle circle-white-small">
+                                    <img src="http://cut2code.test/wp-content/uploads/2021/10/obraz_2021-10-21_154513.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+       <div class="bg-1">
+       </div>
+       <div class="bg-2">
+       </div>
+    </section>
+    <section class="about">
+
+        <div class="container-fluid">
+            <div class="row d-flex justify-content-start align-items-center">
+                <div class="col-8 col-sm-9 col-md-10 col-lg-6 pb-5 p-lg-0 mb-5 m-lg-0" style="index:0;" >
+                    <div class="row d-flex justify-content-end">
+                        <div class="col-md-12 col-lg-10 col-xl-9 fotka">
+                            <img src="http://cut2code.test/wp-content/uploads/2021/10/obraz_2021-10-21_175806.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-xl-3 " style="margin-top:9vh;height:25vh; background-color:white;z-index:1;">
+                    <div class=" p-3 d-flex flex-column align-items-center justify-content-center">
+                        <h3 class="pb-3">Lorem & ipsum </h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, obcaecati magnam neque eligendi molestiae, ullam consectetur dolor facilis ipsam vitae explicabo velit ad sapiente quia ducimus ab, reprehenderit est dolore.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-end" style="padding-top:18vh;">
+                <div class="col d-flex justify-content-center px-3">
+                    <p style="font-size:2.5rem;color:white;">Encourage Your Customers</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section style="position:relative; height:45vh;">
+        <div class="container-fluid" style="position:absolute; top:-15vh;">
+            <div class="row d-flex flex-column align-content-center " >
+                <div class="col-sm-11 col-md-10 col-xl-6 " style="height:100%">
+                   
+                    <div id="carouselExampleDark" class="carousel carousel-dark slide carousel-fade"  data-bs-ride="carousel">
+                        
+                        <div class="carousel-inner  text-dark">
+                            <div class="carousel-item active  text-dark" data-bs-interval="2000">
+                                <img src="http://cut2code.test/wp-content/uploads/2021/10/pexels-mike-1035108-scaled.jpg"  style="border-radius:25px; height:30vh; width:100%; object-fit: cover;  object-position: center;" alt="...">
+                                <div style="margin-bottom:100px">
+                                    <div class="carousel-caption d-none d-md-block " style="background:  rgba(0, 0, 0,0);">
+                                        <p>first vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2000">
+                                <img src="http://cut2code.test/wp-content/uploads/2021/10/pexels-sevenstorm-juhaszimrus-575386-scaled.jpg"  style="border-radius:25px; height:30vh; width:100%; object-fit: cover;  object-position: center;" alt="...">
+                                <div style="margin-bottom:100px">
+                                    <div class="carousel-caption d-none d-md-block" style="background:  rgba(0, 0, 0,0);">
+                                        <p> secondNulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2000">
+                                <img src="http://cut2code.test/wp-content/uploads/2021/10/pexels-daniel-spase-951318-scaled.jpg"  style="border-radius:25px; height:30vh; width:100%; object-fit: cover;  object-position: center;" alt="...">
+                                <div style="margin-bottom:100px">
+                                    <div class="carousel-caption d-none d-md-block" style="background:  rgba(0, 0, 0,0);">
+                                        <p> thirdNulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                  
+                        </div>
+                        <div class="carousel-indicators d-none d-md-flex" style="bottom: -70px!important">
+                            <button style="height:18px; margin:0;"  type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button style="height:18px; margin:0;" type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button style="height:18px; margin:0;" type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section style="z-index:5">
+        <div class="container-fluid" >
+            <div class="row d-flex justify-content-center">
+                <div class=" col-sm-11 col-md-8 col-lg-7 col-xl-4 text-center px-3 p-md-0">
+                    <h1>Frequently Asked Questions</h2>
+                    <div class="accordion-container mt-5 ">
+                        <button class="accordion">Why is simply dummy text of the printing my text of the</button>
+                        <div class="panel">
+                            <p style="padding-top:40px; padding-bottom:30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, iusto?Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-container" >
+                        <button class="accordion">Why is simply dummy text of the printing my text of the</button>
+                        <div class="panel">
+                            <p style="padding-top:40px; padding-bottom:30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-container">
+                        <button class="accordion">Why is simply dummy text of the printing my text of the</button>
+                        <div class="panel">
+                            <p style="padding-top:40px; padding-bottom:30px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+<section class="contact d-none d-md-flex" style="z-index:-1">
+    <div class="container-fluid shade d-flex flex-column justify-content-end " >
+        <div class="row d-flex justify-content-center" style="padding-bottom:5vh;">
+            <div class="col-md-5 col-lg-4 col-xl-3   contact-us d-flex flex-column justify-content-between" style="height:45vh; margin-top:-8vh;">
+                <p style="">Contact <span style="color:#eb6a15">Us</span></p>
+                <div>
+                    <a href="" class="btn-download-contact-us">Download</a>
+                </div>
+            </div>
+            <div class="col-md-5 col-lg-4 col-xl-3   d-flex justify-content-center " style="height:45vh;">
+                <?php echo do_shortcode('[contact-form-7 id="34" title="Formularz 1" html_id="contact-form-1234" html_class="form contact-form"]'); ?>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+        
+    </div>
+</section>
+<section class="contact d-md-none">
+    <div class="container-fluid shade d-flex flex-column justify-content-end " >
+        <div class="row d-flex justify-content-center" style="padding-bottom:5vh;">
+            <div class="col-12 contact-us d-flex flex-column justify-content-between align-items-center" style="height:5vh; margin-bottom:10vh;">
+                <p style="">Contact <span style="color:#eb6a15">Us</span></p>
+                <div>
+                    <a href="" class="btn-download-contact-us">Download</a>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center " style="background-color:#611dd4">
+                <?php echo do_shortcode('[contact-form-7 id="34" title="Formularz 1" html_id="contact-form-1234" html_class="form contact-form"]'); ?>
+                
+            </div>  
+        </div>
+        
+    </div>
+        
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
